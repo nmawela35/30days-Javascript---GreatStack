0be8db7f9bfe6cfa2913b9197049f57a29d4964d
Christopher Mawela <christopher.mawela@gmail.com>
2024-09-29 12:31:52 +0200

    QR Code generator complete

---
+++ 8. QR Code Generator/script.js
@@ -0,0 +1,17 @@
+let imgbox = document.getElementById("img-box")
+let qrImage = document.getElementById("qr-image")
+let qrText = document.getElementById("qr-text")
+
+function generateQr(){
+    if (qrText.value.length > 0){
+
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgbox.classList.add("show-img")
+    } else{
+        qrText.classList.add('error')
+        setTimeout(() =>{
+            qrText.classList.remove('error')
+        },1000)
+    }
+
+}
